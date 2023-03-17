355c27cea8bbf2b6178ee00b9050b2c9b74503c7
Nahir Martinez <nahirmartinez09@gmail.com>
2023-03-17 10:09:32 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,8 @@
+document.querySelector("body").innerHTML = `
+<h1>Practica 1-1</h1>
+    <h2>FrontEnd</h2>
+    <h3>BackEnd</h3>
+    <img src="https://media.istockphoto.com/id/1354667152/photo/the-cute-capybara-in-the-farm-is-taking-a-bath.jpg?s=612x612&w=0&k=20&c=IxROMAoqQ0rDcw4wXh3bCVO0ibh0ZLfHLTfYu646BTw="
+        alt="Capybara">
+        <img src="img/Capybara.jpg" alt="Capybara 2">
+`;
